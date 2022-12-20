fab66d2489b06437bdc727de19691a9fe0167590
Ankit Rai <railucky390@gmail.com>
2022-12-20 11:54:50 +0530

    uses of async await

--- dadJokes/script.js
+++ dadJokes/script.js
@@ -4,16 +4,37 @@ const jokebtn=document.querySelector('.btn');
 
 
 
+//respone.json()//this method that response are in html form and it convert into json form.
 
-const generatejokes=()=>{
-    const setHeader={
+// const generatejokes=()=>{
+    // const setHeader={
+    //     headers:{
+    //         Accept:"application/json"
+    //     }
+    // }
+    // fetch('https://icanhazdadjoke.com/',setHeader).
+    // then((res)=>res.json() ).then((data)=>jokes.innerHTML=data.joke).catch((error)=> console.log(error)
+    // )
+
+                 //async await
+    // async function generateJokes(){} //this for traditional method to write function
+
+    const generatejokes= async()=>{
+        try
+    {const setHeader={
         headers:{
             Accept:"application/json"
-        }
+        } 
     }
-    fetch('https://icanhazdadjoke.com/',setHeader).
-    then((res)=>res.json() ).then((data)=>jokes.innerHTML=data.joke).catch((error)=> console.log(error)
-    )
+     
+   const res=await fetch(
+
+'https://icanhazdadjoke.com/',setHeader);
+  const data =await res.json();
+  jokes.innerHTML=data.joke;}
+  catch(err){
+    console.log(`the error is ${err}`);
+  }
 }
 
 
